fdc07aef34d2515122dc928888d263dd47ff2b4b
Fernando WObeto <fernandowobeto@gmail.com>
2021-06-03 00:43:59 -0300

    firt commit

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# email-blur
+# Email Blur
+
+To install via composer, run:
+
+```
+composer require wobeto/email-blur
+```
+
+Use with default mask:
+
+```php
+<?php
+
+include 'vendor/autoload.php';
+
+use Wobeto\EmailBlur\Blur;
+
+$blur = new Blur('example@test.com');
+    
+$obscured = $blur->make();
+
+var_dump($obscured); // exa***@***.com
+```
+
+Use with total mask changed:
+
+```php
+<?php
+
+include 'vendor/autoload.php';
+
+use Wobeto\EmailBlur\Blur;
+
+$blur = new Blur('example@test.com');
+$blur->setTotalMask(5);
+    
+$obscured = $blur->make();
+
+var_dump($obscured); // exa*****@*****.com
+```
+
+Use with chaining methods:
+
+```php
+<?php
+
+include 'vendor/autoload.php';
+
+use Wobeto\EmailBlur\Blur;
+
+$obscured = (new Blur('example@test.com'))
+    ->setTotalMask(4)
+    ->make();
+
+var_dump($obscured); // exa****@****.com
+```
+
+Enjoy...
---
+++ Wobeto/EmailBlur/Blur.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Wobeto\EmailBlur;
+
+use http\Exception\InvalidArgumentException;
+
+class Blur
+{
+
+    protected string $email;
+    protected int $total_mask = 3;
+
+    public function __construct(string $email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException('E-mail is invalid');
+        }
+
+        $this->email = $email;
+    }
+
+    public function setTotalMask(int $total)
+    {
+        $this->total_mask = $total;
+
+        return $this;
+    }
+
+    public function make()
+    {
+        list($prefix, $domain) = explode('@', $this->email);
+
+        $prefix = $this->maskEmailPrefix($prefix);
+        $domain = $this->maskEmailDomain($domain);
+
+        return $prefix . '@' . $domain;
+    }
+
+    private function maskEmailPrefix(string $prefix): string
+    {
+        $length = strlen($prefix) > 3 ? 3 : 1;
+
+        return (substr($prefix, 0, $length)) . str_repeat('*', $this->total_mask);
+    }
+
+    private function maskEmailDomain(string $domain): string
+    {
+        $domain = explode('.', $domain);
+
+        array_shift($domain);
+
+        return str_repeat('*', $this->total_mask) . '.' . implode('.', $domain);
+    }
+
+}
